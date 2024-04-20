f873cfb2da3f12dafc4444768b6664a60c8e9329
MstrBenja <sanhuez1240@gmail.com>
2024-04-19 22:06:58 -0400

    ejemplo banco

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,68 +28,253 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblNumero = new System.Windows.Forms.GroupBox();
+            this.txtNumeroCredito = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtSueldo = new System.Windows.Forms.TextBox();
+            this.txtMonto = new System.Windows.Forms.TextBox();
+            this.txtCuotas = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblNumeroCredito = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblMensual = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.lblNumero.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(560, 63);
+            this.panel1.TabIndex = 0;
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(290, 125);
+            this.label1.Location = new System.Drawing.Point(258, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(29, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "titulo";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnCalcular);
+            this.groupBox1.Controls.Add(this.txtCuotas);
+            this.groupBox1.Controls.Add(this.txtMonto);
+            this.groupBox1.Controls.Add(this.txtSueldo);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.txtNumeroCredito);
+            this.groupBox1.Location = new System.Drawing.Point(12, 81);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(288, 252);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ingreso de datos";
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.Controls.Add(this.lblTotal);
+            this.lblNumero.Controls.Add(this.lblMensual);
+            this.lblNumero.Controls.Add(this.lblNombre);
+            this.lblNumero.Controls.Add(this.lblNumeroCredito);
+            this.lblNumero.Location = new System.Drawing.Point(306, 81);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(266, 252);
+            this.lblNumero.TabIndex = 2;
+            this.lblNumero.TabStop = false;
+            this.lblNumero.Text = "resultado";
+            // 
+            // txtNumeroCredito
+            // 
+            this.txtNumeroCredito.Location = new System.Drawing.Point(147, 47);
+            this.txtNumeroCredito.Name = "txtNumeroCredito";
+            this.txtNumeroCredito.Size = new System.Drawing.Size(100, 20);
+            this.txtNumeroCredito.TabIndex = 0;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(147, 73);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // txtSueldo
+            // 
+            this.txtSueldo.Location = new System.Drawing.Point(147, 99);
+            this.txtSueldo.Name = "txtSueldo";
+            this.txtSueldo.Size = new System.Drawing.Size(100, 20);
+            this.txtSueldo.TabIndex = 2;
+            // 
+            // txtMonto
+            // 
+            this.txtMonto.Location = new System.Drawing.Point(147, 125);
+            this.txtMonto.Name = "txtMonto";
+            this.txtMonto.Size = new System.Drawing.Size(100, 20);
+            this.txtMonto.TabIndex = 3;
+            // 
+            // txtCuotas
+            // 
+            this.txtCuotas.Location = new System.Drawing.Point(147, 151);
+            this.txtCuotas.Name = "txtCuotas";
+            this.txtCuotas.Size = new System.Drawing.Size(100, 20);
+            this.txtCuotas.TabIndex = 4;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(114, 206);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(45, 81);
+            this.label2.Location = new System.Drawing.Point(54, 80);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "nombre";
             // 
-            // button1
+            // label3
             // 
-            this.button1.Location = new System.Drawing.Point(115, 221);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "numero credito";
             // 
-            // textBox1
+            // label4
             // 
-            this.textBox1.Location = new System.Drawing.Point(423, 261);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 3;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "sueldo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(57, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "monto credito";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(57, 157);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "cuotas";
+            // 
+            // lblNumeroCredito
+            // 
+            this.lblNumeroCredito.AutoSize = true;
+            this.lblNumeroCredito.Location = new System.Drawing.Point(130, 59);
+            this.lblNumeroCredito.Name = "lblNumeroCredito";
+            this.lblNumeroCredito.Size = new System.Drawing.Size(77, 13);
+            this.lblNumeroCredito.TabIndex = 0;
+            this.lblNumeroCredito.Text = "numero credito";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(133, 80);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(42, 13);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "nombre";
+            // 
+            // lblMensual
+            // 
+            this.lblMensual.AutoSize = true;
+            this.lblMensual.Location = new System.Drawing.Point(133, 105);
+            this.lblMensual.Name = "lblMensual";
+            this.lblMensual.Size = new System.Drawing.Size(99, 13);
+            this.lblMensual.TabIndex = 2;
+            this.lblMensual.Text = "valor pago mensual";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(123, 151);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(88, 13);
+            this.lblTotal.TabIndex = 3;
+            this.lblTotal.Text = "valor total credito";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.lblNumero.ResumeLayout(false);
+            this.lblNumero.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtCuotas;
+        private System.Windows.Forms.TextBox txtMonto;
+        private System.Windows.Forms.TextBox txtSueldo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtNumeroCredito;
+        private System.Windows.Forms.GroupBox lblNumero;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblMensual;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblNumeroCredito;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,99 @@ namespace ejemploGit
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            string numeroCredito = txtNumeroCredito.Text;
+            int numero = 0;
+            try
+            {
+                numero = Convert.ToInt32(numeroCredito);
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                txtNumeroCredito.Text = "";
+            }
+            
+            lblNumeroCredito.Text = numero.ToString();
+
+            string nombre = txtNombre.Text;
+            if (nombre.Length > 0) 
+            {
+                MessageBox.Show("Debe ingresar un valor valido");
+            }
+
+            string sueldo = txtSueldo.Text;
+            double sueldo_dou = 0;
+            try
+            {
+                sueldo_dou = Convert.ToDouble(sueldo);
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                txtSueldo.BackColor = Color.Red;
+            }
+
+            string montoCredito = txtMonto.Text;
+            double monto= 0;
+            try
+            {
+                monto = Convert.ToInt32(montoCredito);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Monto debe ser valor de tipo numerico");
+               
+            }
+
+            string cuotas = txtCuotas.Text;
+            int cuotas_int= 0;
+            try
+            {
+                cuotas_int = Convert.ToInt32(cuotas);
+            }catch(Exception ex)
+            {
+                MessageBox.Show("valor debe ser numero");
+                txtCuotas.Text = "";
+                txtCuotas.BackColor = Color.Red;
+
+            }
+
+            double maximoCredito = sueldo_dou * 1.5;
+
+
+            if(monto<= maximoCredito)
+            {
+                if (cuotas_int >= 6)
+                {
+                    float tasaInteres = 0;
+                    double valorMensual = 0;
+                    if(cuotas_int <= 48)
+                    {
+                        tasaInteres = 1.85f / 12;
+
+                    }
+                    else
+                    {
+                        tasaInteres = 3.0f / 12;
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("NO PUEDE SER MENOR A 6");
+                }
+            }
+            else
+            {
+                MessageBox.Show("ud no puede solicitar un monto tan grande");
+            }
+
+        }
     }
 }
